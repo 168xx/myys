45feb180c8ad36b25404b5a349267ea3f36ab567
baidu <baidu@baidu.com>
2024-12-31 00:21:33 +0800

    up

---
+++ ysxl/bb/lib/美.js
@@ -0,0 +1,7 @@
+{
+            "请求头": "User-Agent$MOBILE_UA",
+             "编码": "UTF-8",
+             "副标题":"✨轩蓓✨+text-right\">&&</span>",
+              "播放标题": "✨轩蓓👉+>&&</span>",
+             "分类url":"https://www.mydys1.com/vodshow/{cateId}-{area}--{class}-{lang}-{letter}---{catePg}---{year}.html",
+         "分类": "电影$1#连续剧$2#综艺$3#动漫$4#短剧$5"}
